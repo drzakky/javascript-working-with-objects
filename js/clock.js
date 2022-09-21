532dea2c809ae0547bd0ab6098b2c1259b8f9ee3
Isaac Crawford <isaac-robert.crawford@skybettingandgaming.com>
2022-09-21 10:27:49 +0100

    working clock face

--- js/clock.js
+++ js/clock.js
@@ -4,15 +4,22 @@
  let myDate = new Date();
  let myNode = document.createElement('div');
  document.getElementById('digitalClock').appendChild(myNode)
-let rotSeconds = -90
-let rotMinutes = -90
-let rotHours = -90
- let second = 0
- let minute = 0
- let hour = 0
-
- 
  myNode.innerHTML = myDate.toTimeString().substr(0,8);
+
+ let second = Math.floor(myDate.toTimeString().substr(6,8).substr(0,2))
+
+
+ let minute = Math.floor(myDate.toTimeString().substr(3,5).substr(0,2))
+ let hour = Math.floor(myDate.toTimeString().substr(0,2))
+let rotSeconds = (second * 6) - 90
+let rotMinutes = (minute * 6) - 90
+let rotHours = (hour * 30) - 90
+document.getElementById('minHand').style.transform = 'rotate('+rotMinutes+'deg)'
+document.getElementById('secondHand').style.transform = 'rotate('+rotSeconds+'deg)'
+document.getElementById('hrHand').style.transform = 'rotate('+rotHours+'deg)'
+ 
+ 
+
 
 var updateTime = function(){
     var myDate = new Date();
@@ -27,7 +34,7 @@ var updateTime = function(){
     second += 1
     rotSeconds = (second * 6) - 90;
     document.getElementById('secondHand').style.transform = 'rotate('+rotSeconds+'deg)'
- },1000) 
+   },1000) 
 
  let minutes = setInterval(function(){
     minute+= 1
@@ -38,11 +45,12 @@ var updateTime = function(){
 
 let hours = setInterval(function(){
     hour+= 1
+    rotHours = (hour * 3.75) - 90;
     console.log(hour)
-    rotHours = (hour * 6) - 90;
     document.getElementById('hrHand').style.transform = 'rotate('+rotHours+'deg)'
- },3600000) 
-
+ },450000)
+ 
+//  360000
 
  
 
